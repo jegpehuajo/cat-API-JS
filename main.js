530a6293599552ad9f0d30707b0a7fb43f08f084
Jorge E Gomez <jeg1986@gmail.com>
2023-08-05 10:36:36 -0300

    Cambie la conexión de la API, imagen random presionando boton e imagen.

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const URL = 'https://api.thecatapi.com/v1/images/search';
 
-fetch(URL)
-  .then(res => res.json())
-  .then(data => {
-    const img = document.querySelector('img');
+const img = document.querySelector("img");
+
+const btnAleatorio = document.querySelector("button");
+
+async function getImg() {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
     img.src = data[0].url;
-  });
+  } catch (error) {
+    console.error("Error al obtener la imagen");
+  }
+}
+
+img.addEventListener("click",getImg);
+btnAleatorio.addEventListener("click",getImg);
+
+getImg();
+
+// Sintaxis de promesas.
+// fetch(URL)
+//   .then(res => res.json())
+//   .then(data => {
+//     const img = document.querySelector('img');
+//     img.src = data[0].url;
+//   });
+
