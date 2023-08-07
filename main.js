ea84228d684d006d8e052e1a9fd8346fe5f5f106
Jorge E Gomez <jeg1986@gmail.com>
2023-08-07 17:09:06 -0300

    Agregue spanError para saber el tipo de error.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=1&api_key=live_DY2e8NqCNT2B5zkxPa4rGuWuyIr5c3RP0iOYhn3plIVKNjbR7Z9u24PWTQXP9vhl';
-const URL_FAV = 'https://api.thecatapi.com/v1/favourites?api_key=live_DY2e8NqCNT2B5zkxPa4rGuWuyIr5c3RP0iOYhn3plIVKNjbR7Z9u24PWTQXP9vhl';
+const URL_FAV = 'https://api.thecatapi.com/v1/favourites?limit=3&api_key=live_DY2e8NqCNT2B5zkxPa4rGuWuyIr5c3RP0iOYhn3plIVKNjbR7Z9u24PWTQXP9vhl';
 
 const img = document.querySelector("img");
 const btnAleatorio = document.querySelector("#otro_gato");
+const btnGuardar = document.querySelector("#guardar_fav");
+const spanError = document.querySelector("#spanError");
 
 async function getImg() {
   try {
@@ -25,8 +27,43 @@ async function getFav() {
   }
 }
 
+async function saveFav() {
+  const response = await fetch(URL_FAV, {
+    method:'POST',
+    headers: {
+      'Content-Type':'application/json',
+    },
+    body: JSON.stringify({
+      image_id:'12'
+    }),
+  });
+
+  const data = await response.json();
+
+  if (response.status !== 200) {
+    spanError.innerHTML = "Hay un error: " + response.status + data.message;
+  } else {
+    data.forEach(gato => {
+      const section = document.getElementById('favoritesCat');
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode("Sacar de favoritos");
+
+      btn.appendChild(btnText);
+      img.src = gato.image.url;
+      
+      article.appendChild(img);
+      article.appendChild(btn);
+
+      section.appendChild(article);
+    });
+  }
+}
+
 img.addEventListener("click",getImg);
 btnAleatorio.addEventListener("click",getImg);
+btnGuardar.addEventListener("click",saveFav);
 
 getImg();
 getFav();
