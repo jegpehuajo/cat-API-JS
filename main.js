89eb5043144e17402fb76ab82f1b217fdb21e4b6
Jorge E Gomez <jeg1986@gmail.com>
2023-08-09 08:33:47 -0300

    Fix - function saveFav

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=1&api_key=live_DY2e8NqCNT2B5zkxPa4rGuWuyIr5c3RP0iOYhn3plIVKNjbR7Z9u24PWTQXP9vhl';
+const URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?api_key=live_DY2e8NqCNT2B5zkxPa4rGuWuyIr5c3RP0iOYhn3plIVKNjbR7Z9u24PWTQXP9vhl';
 const URL_FAV = 'https://api.thecatapi.com/v1/favourites?api_key=live_DY2e8NqCNT2B5zkxPa4rGuWuyIr5c3RP0iOYhn3plIVKNjbR7Z9u24PWTQXP9vhl';
+//const URL_FAV_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_DY2e8NqCNT2B5zkxPa4rGuWuyIr5c3RP0iOYhn3plIVKNjbR7Z9u24PWTQXP9vhl`;
+//const API_KEY = 'live_DY2e8NqCNT2B5zkxPa4rGuWuyIr5c3RP0iOYhn3plIVKNjbR7Z9u24PWTQXP9vhl';
 
 const img = document.querySelector("img");
 const btnAleatorio = document.querySelector("#otro_gato");
 const btnGuardar = document.querySelector("#guardar_fav");
-const spanError = document.querySelector("#spanError");
+const spanError = document.getElementById("spanError");
 
 async function getImg() {
   try {
@@ -13,6 +15,7 @@ async function getImg() {
     img.src = data[0].url;
   } catch (error) {
     console.error("Error al obtener la imagen");
+    spanError.innerText = "Hubo un error: " + response.status + data.message;
   }
 }
 
@@ -31,33 +34,54 @@ async function getFav() {
       const article = document.createElement('article');
       const img = document.createElement('img');
       const btn = document.createElement('button');
+      btn.id = "sacar_fav";
       const btnText = document.createTextNode("Sacar de favoritos");
 
       btn.appendChild(btnText);
-      img.src = gato.image.url;
-      
+      //img.src = gato.image.url;
+      img.src = gato[0].data.url;
       article.appendChild(img);
       article.appendChild(btn);
-
+      btn.onclick = () => deleteFav(id);
       section.appendChild(article);
     });
   }
   } catch {
-    console.error("Error al obtener la imagen en favoritos.");
+    console.warn("Error al obtener la imagen en favoritos.");
+    spanError.innerText = "Hubo un error: " + response.status + data.message;
   }
 }
 
-async function saveFav() {
+async function saveFav(id) {
   const response = await fetch(URL_FAV, {
     method:'POST',
     headers: {
       'Content-Type':'application/json',
     },
     body: JSON.stringify({
-      image_id:'12'
+      image_id:`${id}`
     }),
   });
+  const data = await response.json();
+  console.log("Save");
+  console.log(response)
 }
+
+// async function deleteFav(id) {
+//   const response = await fetch(URL_FAV_DELETE(id), {
+//     method: 'DELETE',
+//     headers: {
+//       "x-api-key": `{API_KEY}`,
+//     },
+//   });
+//   const data = await response.json();
+
+//   if(response.status !== 200) {
+//     spanError.innerHTML = "Hubo un error: " + response.status + data.message;
+//   } else {
+//     console.log("Se elimino de favoritos.");
+//   }
+// }
 
 img.addEventListener("click",getImg);
 btnAleatorio.addEventListener("click",getImg);
